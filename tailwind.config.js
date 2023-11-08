c35d68fd74113dd7453101c80d701bfab7c7e44c
Flavio <flaviofilholm10@gmail.com>
2023-11-08 15:29:13 -0300

    updating

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,15 @@ module.exports = {
       height: {
         '700': "95%",
       },
+      width: {
+        '80': '80%',
+        '90': '90%',
+        '100': '100%',
+      },
       colors: {
         'custom-gray': '#181818',
+        'input': '#232323',
+        'custom-gold': '#ffe919',
       },
       borderRadius:{
         'custom': '1.25em',
